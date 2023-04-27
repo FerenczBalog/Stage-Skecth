67ef4523df9656c5e2918404b0e3049a4cc0c39e
ferenczbalog <balogferencz.artist@gmail.com>
2023-04-27 22:48:05 +0300

    > updated the json file > added rider asset > added footer asset

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import Header from "./assets/Header.mjs";
 import Section from "./assets/Section.mjs";
 import DataHandler from "./assets/dataHandler.mjs"
+import Footer from "./assets/footer.mjs";
 
 class App {
 
@@ -19,6 +20,7 @@ class App {
         `
             ${new Header(this.data).render()}
             ${new Section(this.data).render()}
+            ${new Footer().render()}
         `;
 
         documentFragment.appendChild(main);
