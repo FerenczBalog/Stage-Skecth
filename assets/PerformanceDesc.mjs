8e96b0e537255cc0eccbafa2ffafc8cd56271851
ferenczbalog <balogferencz.artist@gmail.com>
2023-04-27 22:33:10 +0300

    .

--- assets/PerformanceDesc.mjs
+++ assets/PerformanceDesc.mjs
@@ -10,7 +10,14 @@ export default class PerformanceDesc {
             <div class="d-flex justify-content-center flex-reverse mt-5">
                 <div class="col-4">
                     <h2>${this.props.name}</h2>
-                    <p>${this.props.description}</p>
+                    <p><strong>Felújítva:</strong> ${this.props.year}</p>
+                    <p><strong>Rendező:</strong> ${this.props.director}</p>
+                    <p><strong>Előadók:</strong> ${this.props.actors}</p>
+                    <p><strong>Ügyelő:</strong> ${this.props.stageDir}</p>
+                    <p><strong>Hangosító:</strong> ${this.props.sound}</p>
+                    <p><strong>Világosító:</strong> ${this.props.light}</p>
+                    <p><strong>Díszítők:</strong> ${this.props.stageTech}</p>
+                    <p><strong>Szállítás:</strong> ${this.props.transport}</p>
                 </div>
 
                 <div class="col-4">
