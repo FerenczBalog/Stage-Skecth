5eb77da268335c8c19db2c11bae071115c0ce601
Balog Ferencz <90491641+FerenczBalog@users.noreply.github.com>
2023-05-15 07:22:04 +0300

    Update rider.mjs

--- assets/rider.mjs
+++ assets/rider.mjs
@@ -10,7 +10,7 @@ export default class Rider {
                 <h2>${this.props.name}</h2>
                 <div style="font-size: 0.8rem; margin-right: 3rem">
                     <p><strong>Bemutató:</strong> ${this.props.year}</p>
-                    <p><strong>Rendez:</strong> ${this.props.director}</p>
+                    <p><strong>Rendező:</strong> ${this.props.director}</p>
                     <p><strong>Előadók:</strong> ${this.props.actors}</p>
                     <p><strong>Ügyelő:</strong> ${this.props.stageDir}</p>
                     <p><strong>Hangosító:</strong> ${this.props.sound}</p>
